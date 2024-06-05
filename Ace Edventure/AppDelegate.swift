8fa8b14c7322775d6eabd1570677cac74b5a1b69
rifauzh <rifafauziah.rf15@gmail.com>
2024-06-05 15:51:45 +0800

    update noti

--- Ace Edventure/AppDelegate.swift
+++ Ace Edventure/AppDelegate.swift
@@ -15,13 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     var window: UIWindow?
+    var notificationHandler: NotificationHandler?
      
     let gcmMessageKey = "gcm_Message_ID"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+         FirebaseApp.configure()
         
-         UNUserNotificationCenter.current().delegate = self
+         notificationHandler = NotificationHandler()
+        
          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
          UNUserNotificationCenter.current().requestAuthorization(
          options: authOptions,
@@ -36,46 +38,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-     Messaging.messaging().apnsToken = deviceToken
-     print("APNs token retrieved: \(deviceToken)")
+         Messaging.messaging().apnsToken = deviceToken
+         print("APNs token retrieved: \(deviceToken)")
      }
      
      func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-     print("Failed to register for remote notifications with error: \(error)")
+         print("Failed to register for remote notifications with error: \(error)")
      }
-     }
-     
-     // MARK: - UNUserNotificationCenterDelegate
-     extension AppDelegate: UNUserNotificationCenterDelegate {
-     // Receive displayed notifications for iOS 10 devices.
-     func userNotificationCenter(_ center: UNUserNotificationCenter,
-     willPresent notification: UNNotification,
-     withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-     let userInfo = notification.request.content.userInfo
-     print(userInfo)
-     completionHandler([[.alert, .sound]])
-     }
-     
-     func userNotificationCenter(_ center: UNUserNotificationCenter,
-     didReceive response: UNNotificationResponse,
-     withCompletionHandler completionHandler: @escaping () -> Void) {
-     let userInfo = response.notification.request.content.userInfo
-     print(userInfo)
-     completionHandler()
-     }
-     }
+}
      
      // MARK: - MessagingDelegate
      extension AppDelegate: MessagingDelegate {
      func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
      
-     print("Firebase registration token: \(String(describing: fcmToken))")
-     let dataDict: [String: String] = ["token": fcmToken ?? ""]
-     NotificationCenter.default.post(
-     name: Notification.Name("FCMToken"),
-     object: nil,
-     userInfo: dataDict
-     )
+         print("Firebase registration token: \(String(describing: fcmToken))")
+         let dataDict: [String: String] = ["token": fcmToken ?? ""]
+         NotificationCenter.default.post(
+             name: Notification.Name("FCMToken"),
+             object: nil,
+             userInfo: dataDict
+         )
      }
-    
 }
--- Ace Edventure/NotificationHandler.swift
+++ Ace Edventure/NotificationHandler.swift
@@ -5,4 +5,47 @@
 //  Created by Rifa Fauziah on 05/06/2024.
 //
 
-import Foundation
+import UIKit
+import UserNotifications
+import Firebase
+
+class NotificationHandler: NSObject, UNUserNotificationCenterDelegate {
+
+    override init() {
+        super.init()
+        UNUserNotificationCenter.current().delegate = self
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+
+    // This method handles the user's interaction with the notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+
+        // extract the body of the notification from the UNNotificationResponse object, else if not successful retreive from UserInfo (is a dictionary containing additional data sent with the notification)
+         // but "body = notification.request.content.body as String?" works
+        if let body = response.notification.request.content.body as String?{
+            print("Notification body (content): \(body)")
+            redirectToGoogle(body)
+        } else if let body = userInfo["body"] as? String {
+            print("Notification body (userInfo): \(body)")
+            redirectToGoogle(body)
+        }
+
+        completionHandler()
+    }
+
+    private func redirectToGoogle(_ body: String) {
+        if let url = URL(string: body), UIApplication.shared.canOpenURL(url) {
+            DispatchQueue.main.async {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        } else {
+            print("Notification body is not a valid URL: \(body)")
+        }
+    }
+}
+
+
